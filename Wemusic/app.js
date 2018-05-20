8b005b95356a66268f1279d5904563e728c73a42
Yujie.zhou <yjzhouecnu@163.com>
2018-05-20 17:21:37 +0800

    0520

--- Wemusic/app.js
+++ Wemusic/app.js
@@ -1,6 +1,8 @@
 //app.js
+
 App({
   onLaunch: function () {
+
     // 展示本地存储能力
     var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
@@ -8,11 +10,29 @@ App({
 
     // 登录
     wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+      success: function (res) {
+        if (res.code) {
+          //发起网络请求
+          wx.request({
+            url: 'https://www.seanxu.club/apiweixinqqcom?code=' + res.code,
+            method: 'GET',
+            //请求成功
+            success: (res) => {
+              console.log(res.data.openid);
+              this.globalData.openid = res.data.openid;
+              console.log(openid);
+            },
+            fail: (res) => {
+              console.log("fail");
+              console.log(res.errMsg);
+            },
+          });
+
+        }
       }
     })
     // 获取用户信息
+    
     wx.getSetting({
       success: res => {
         if (res.authSetting['scope.userInfo']) {
@@ -21,7 +41,7 @@ App({
             success: res => {
               // 可以将 res 发送给后台解码出 unionId
               this.globalData.userInfo = res.userInfo
-
+              console.log(this.globalData.userInfo)
               // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
               // 所以此处加入 callback 以防止这种情况
               if (this.userInfoReadyCallback) {
@@ -34,6 +54,7 @@ App({
     })
   },
   globalData: {
+    openid: "adfadfad",
     userInfo: null
   }
 })
--- Wemusic/pages/test/test.js
+++ Wemusic/pages/test/test.js
@@ -1,35 +1,42 @@
 // pages/test/test.js
 var util = require('../../utils/util.js');  
+var total_day=0;
+var continue_day=0;
+var max_continue_day=0;
+var todayDate=0;
+var sign_day=0;
+var rank=0;
+var app = getApp();
 Page({
 
   /**
    * 页面的初始数据
    */
-
   data: {
     sign:"今 日 签 到",
-
     btnColor:"#f1c74a",
-    conDays:"10",
-    sumDays:"20",
-    grade:"6",
-    days:"1024",
-    signDate:"",
-    disabled:false,
+    conDays:"0",
+    maxDays:"0",
+    grade:"0",
+    days:0,
+    signDate:"00000000",
+    btnDisabled:false,
     url_select: 'https://www.seanxu.club/music?operation=select&id=',
     url_update: 'https://www.seanxu.club/music?operation=update&id=',
   },
   signBtn: function (e) {
+    total_day = total_day + 1;
+    continue_day = continue_day + 1;
+    max_continue_day = max_continue_day + 1;
     this.setData({
       sign: "已 签 到",
       btnColor: "#faedbd",
-      disabled: true
+      btnDisabled: true,
     })
-    todayDate = util.formatTime(new Date())
     wx.request({
       // 请求服务器 url 发送签到请求
-      url: this.data.url_update + app.globalData.openid + '&signDate=' + todayDate,
-      //点击签到后sign=1
+      url: this.data.url_update + app.globalData.openid + '&item_name=sign_day&item_value='+todayDate,
+      //url: this.data.url_update + '0'+'&item_name=sign_day&item_value='+todayDate,
       method: 'GET',
       //请求成功
       success: (res) => {
@@ -41,122 +48,128 @@ Page({
         console.log(res.errMsg);
       },
     });
+    wx.request({
+      // 请求服务器 url 发送签到请求
+      url: this.data.url_update + app.globalData.openid + '&item_name=total_day&item_value=' +total_day,
+      //url: this.data.url_update + '0' + '&item_name=total_day&item_value=' +total_day,
+      //点击签到后sign=1
+      method: 'GET',
+      //请求成功
+      success: (res) => {
+        // 返回状态码 200
+        console.log("success");
+      },
+      fail: (res) => {
+        console.log("fail");
+        console.log(res.errMsg);
+      },
+    });
+    wx.request({
+      // 请求服务器 url 发送签到请求
+      url: this.data.url_update + app.globalData.openid + '&item_name=max_continue_day&item_value=' + max_continue_day,
+      //url: this.data.url_update + '0' + '&item_name=max_continue_day&item_value=' + max_continue_day,
+      method: 'GET',
+      //请求成功
+      success: (res) => {
+        // 返回状态码 200
+        console.log("success");
+      },
+      fail: (res) => {
+        console.log("fail");
+        console.log(res.errMsg);
+      },
+    });
+    wx.request({
+      // 请求服务器 url 发送签到请求
+      url: this.data.url_update + app.globalData.openid + '&item_name=continue_day&item_value=' + continue_day,
+      //url: this.data.url_update + '0' + '&item_name=continue_day&item_value=' + continue_day,
+      //点击签到后sign=1
+      method: 'GET',
+      //请求成功
+      success: (res) => {
+        // 返回状态码 200
+        console.log("success");
+      },
+      fail: (res) => {
+        console.log("fail");
+        console.log(res.errMsg);
+      },
+    });
+    wx.request({
+      // 请求服务器 url 发送签到请求
+      url: this.data.url_select + app.globalData.openid,
+      //url: this.data.url_select + '0',
+      method: 'GET',
+      //请求成功
+      success: (res) => {
+        // 返回状态码 200
+        console.log(res.data);
+        this.setData({
+          conDays: res.data.continue_day,
+          maxDays: res.data.max_continue_day,
+          sumDays: res.data.total_day,
+          grade: res.data.rank,
+          days: res.data.total_day,
+          signDate: res.data.sign_day
+        })
+      },
+      fail: (res) => {
+        console.log("fail");
+        console.log(res.errMsg);
+      },
+    });
   },
   /**
    * 生命周期函数--监听页面加载
    */
   signInit:function(){
+    todayDate = util.formatTime(new Date())
     wx.request({
       // 请求服务器 url 发送签到请求
-      url: this.data.url_update + app.globalData.openid + '&signDate',
+      url: this.data.url_select + app.globalData.openid,
+      //url: this.data.url_select + '0',
       method: 'GET',
       //请求成功
       success: (res) => {
         // 返回状态码 200
         console.log(res.data);
-        that.setData({
-          signDate: res.data.signDate,
+        total_day=res.data.total_day;
+        continue_day=res.data.continue_day;
+        max_continue_day=res.data.max_continue_day;
+        sign_day=res.data.sign_day;
+        rank = parseInt(total_day / 10)
+        this.setData({
+          conDays: continue_day,
+          maxDays: max_continue_day,
+          sumDays: total_day,
+          grade: rank,
+          days: total_day,
+          signDate: sign_day
         })
+        if (sign_day == todayDate) {
+          this.setData({
+            sign: "已 签 到",
+            btnColor: "#faedbd",
+            btnDisabled: true
+          })
+        }
+        else {
+          this.setData({
+            sign: "今 日 签 到",
+            btnColor: "#f1c74a",
+            btnDisabled: false
+          })
+        }
       },
       fail: (res) => {
         console.log("fail");
         console.log(res.errMsg);
       },
     });
-    if(signData == util.formatTime(new Date())) {
-      this.setData({
-        sign: "已 签 到",
-        btnColor: "#faedbd",
-        disabled: true
-      })
-    }
-  },
-  conDaysInit: function(){
-    wx.request({
-      // 请求服务器 url 发送签到请求
-      url: this.data.url_select + app.globalData.openid + '&conDays',
-      //查询连续天数
-      method: 'GET',
-      //请求成功
-      success: (res) => {
-        // 返回状态码 200
-        console.log(res.data);
-        that.setData({
-          conDays: res.data.conDays,
-        })
-      },
-      fail: (res) => {
-        console.log("fail");
-        console.log(res.errMsg);
-      },
-    });
-  },
-  sumDaysInit: function () {
-    wx.request({
-      // 请求服务器 url 发送签到请求
-      url: this.data.url_select + app.globalData.openid + '&sumDays',
-      //查询连续天数
-      method: 'GET',
-      //请求成功
-      success: (res) => {
-        // 返回状态码 200
-        console.log(res.data);
-        that.setData({
-          sumDays: res.data.sumDays,
-        })
-      },
-      fail: (res) => {
-        console.log("fail");
-        console.log(res.errMsg);
-      },
-    });
-  },
-  daysInit: function () {
-    wx.request({
-      // 请求服务器 url 发送签到请求
-      url: this.data.url_select + app.globalData.openid + '&days',
-      //查询连续天数
-      method: 'GET',
-      //请求成功
-      success: (res) => {
-        // 返回状态码 200
-        console.log(res.data);
-        that.setData({
-          days: res.data.days,
-        })
-      },
-      fail: (res) => {
-        console.log("fail");
-        console.log(res.errMsg);
-      },
-    });
-  },
-  gradeInit: function () {
-    wx.request({
-      // 请求服务器 url 发送签到请求
-      url: this.data.url_select + app.globalData.openid + '&grade',
-      //查询连续天数
-      method: 'GET',
-      //请求成功
-      success: (res) => {
-        // 返回状态码 200
-        console.log(res.data);
-        that.setData({
-          grade: res.data.grade,
-        })
-      },
-      fail: (res) => {
-        console.log("fail");
-        console.log(res.errMsg);
-      },
-    });
+    
   },
   onLoad: function (options) { 
-    this.signInit()
-    this.conDaysInit()
-    this.sumDaysInit()
-    this.gradeInit()
-    this.daysInit()   
+    this.signInit() 
   },
 
   /**
